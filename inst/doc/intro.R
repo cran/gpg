b5a0e443ce569ef4bec41ccc518a74e09907d7f8
Jeroen Ooms <jeroen@berkeley.edu>
2021-11-17 12:00:02 +0000

    version 1.2.4

--- inst/doc/intro.R
+++ inst/doc/intro.R
@@ -70,7 +70,7 @@ unlink(c("sig.gpg", "clearsig.gpg"))
 
 ## ---- message=FALSE---------------------------------------------------------------------------------------------------
 # take out the spaces
-johannes <- "E19F5F87128899B192B1A2C2AD5F960A256A04AF"
+johannes <- "0xB8F25A8A73EACF41"
 gpg_recv(johannes)
 
 ## ---------------------------------------------------------------------------------------------------------------------
@@ -79,8 +79,8 @@ gpg_list_signatures(johannes)
 ## ---------------------------------------------------------------------------------------------------------------------
 # Verify the file
 library(curl)
-curl_download('https://cran.r-project.org/bin/linux/debian/buster-cran35/Release', 'Release')
-curl_download('https://cran.r-project.org/bin/linux/debian/buster-cran35/Release.gpg','Release.gpg')
+curl_download('https://cran.r-project.org/bin/linux/debian/bullseye-cran40/Release', 'Release')
+curl_download('https://cran.r-project.org/bin/linux/debian/bullseye-cran40/Release.gpg','Release.gpg')
 gpg_verify('Release.gpg', 'Release')
 
 ## ---- echo = FALSE----------------------------------------------------------------------------------------------------
